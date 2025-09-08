6d7d3063d2470bac646adea7bda8c84243e2b40b
sheemia <chenjunyang2007@gmail.com>
2025-09-08 21:41:06 +0800

    study cpp

---
+++ cpp/setprecision.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double a=sqrt(3);
+    cout<<fixed<<setprecision(3)<<a<<endl;
+    return 0;
+}
---
+++ cpp/test.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin>>n;
+    if(n%2!=0) cout<<3*n+1;
+    return 0;
+}
