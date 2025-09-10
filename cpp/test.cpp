0e87d9a69d26dca76bf04d44e2ca9e2e9cc19f79
sheemia <chenjunyang2007@gmail.com>
2025-09-10 22:18:20 +0800

    study app

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -1,13 +1,30 @@
-#include<iostream>
-#include<cmath>
-#include<iomanip>
+#include<iostream>//cout cin endl
+#include<cmath>//sqrt fabs
+#include<iomanip>//fix setprecision
+#include<algorithm>//max sort
+#include<cstdlib>//abs
+
 using namespace std;
+
+bool has4(int x)
+{
+    while(x>0)
+    {
+        if(x%10==4) return true;
+        else x/=10;
+    }
+    return false;
+}
 
 int main() 
 {
     int n;
     cin>>n;
-    if(n%2!=0) cout<<3*n+1<<endl;
-    else cout<<n/2<<endl;
+    for(int i=1;i<=n;i++)
+    {
+        if(i%4==0) continue;
+        if(has4(i)) continue;
+        cout<<i<<endl;
+    }
     return 0;
 }
