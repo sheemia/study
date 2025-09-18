95749e4b0e396f861f170dc91ae540426e869bd1
mac <chenjunyang2007@gmail.com>
2025-09-18 22:28:15 +0800

    do practice problems

--- cpp/test.cpp
+++ cpp/test.cpp
@@ -3,13 +3,28 @@
 #include<iomanip>//fix setprecision
 #include<algorithm>//max sort fill
 #include<cstdlib>//abs
-
+ 
 using namespace std;
-
-int main() 
+ 
+int main()
 {
-    int N;
-    c>>N;
-    
+    string s1,s2;
+    cin>>s1>>s2;
+
+    if(s1=="elephant"&&s2=="tiger"||
+    s1=="tiger"&&s2=="cat"||
+    s1=="cat"&&s2=="mouse"||
+    s1=="mouse"&&s2=="elephant")
+    {
+        cout<<"win";
+    }
+    else if(s2=="elephant"&&s1=="tiger"||
+    s2=="tiger"&&s1=="cat"||
+    s2=="cat"&&s1=="mouse"||
+    s2=="mouse"&&s1=="elephant")
+    {
+        cout<<"lose";
+    }
+    else cout<<"tie";
     return 0;
 }
